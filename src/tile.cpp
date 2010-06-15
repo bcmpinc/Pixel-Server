b118cdc76ae8598eac13a96e57645ea90f26cf2d
bcmpinc <bcmpinc@users.sourceforge.net>
2010-06-15 16:59:35 +0200

    Improved noise

--- src/tile.cpp
+++ src/tile.cpp
@@ -12,46 +12,60 @@ inline int fist(float f) {
 inline int rgb(int r, int g, int b) {
 	return CLAMP(r)|CLAMP(g)<<8|CLAMP(b)<<16;
 }
-inline int rgb(double r, double g, double b) {
+inline int rgb(float r, float g, float b) {
 	return rgb(fist(r*255),fist(g*255),fist(b*255));
 }
 
 // Noise functions
-int p[256];
-
+#define NOISE_LENGTH 512
+int p[NOISE_LENGTH];
+float g[NOISE_LENGTH];
 void tile_init(int argc, char ** argv) {
 	srand(1337);
-	for (int i=0; i<256; i++) {
+	for (int i=0; i<NOISE_LENGTH; i++) {
 		p[i]=i;
+		g[i]=rand() / (RAND_MAX/2.0f) - 1;
 	}
-	for (int i=0; i<256; i++) {
-		int j = rand()&0xff;
+	for (int i=0; i<NOISE_LENGTH; i++) {
+		int j = rand() % NOISE_LENGTH;
 		p[i]^=p[j]^=p[i]^=p[j];
 	}
 }
 
 float noise(int x, int y) {
-	return p[(x+p[y&0xff])&0xff]/255.0f;
+	return g[(x + p[y & 0xff]) & 0xff];
 }
 
-float lerp(float t, float a, float b) {
-	return a*(1-t)+b*t;
+float lerp3(float t, float a, float b, float c) {
+	float st = t * t;
+	return (c*(st+2*t+1) + b*(5-st) + a*(st-2*t+1)) / (7+st);
 }
 
 float noise(float x, float y) {
-	int px = fist(x);
-	int py = fist(y);
-	if (x<px) px--;
-	if (y<py) py--;
-	x-=px;
-	y-=py;
-	return lerp(y,
-		lerp(x,noise(px,py),  noise(px+1,py)),
-		lerp(x,noise(px,py+1),noise(px+1,py+1)));
+	int   px = fist(x), py = fist(y);
+	      x -= px;      y -= py;
+
+	float a = noise(px-1, py-1);
+	float b = noise(px  , py-1);
+	float c = noise(px+1, py-1);
+	float d = noise(px-1, py  );
+	float e = noise(px  , py  );
+	float f = noise(px+1, py  );
+	float g = noise(px-1, py+1);
+	float h = noise(px  , py+1);
+	float i = noise(px+1, py+1);
+	
+	x*=2;
+	y*=2;
+	float k=lerp3(x,a,b,c);
+	float l=lerp3(x,d,e,f);
+	float m=lerp3(x,g,h,i);
+	
+	return  lerp3(y,k,l,m);
 }
 
 float f(float x, float y) {
-	return .3/noise(x,y);
+	return noise(x,y);
 }
 
 float fs(float x, float y) {
@@ -59,11 +73,12 @@ float fs(float x, float y) {
 }
 
 int grad(float f) {
+	f=.5-f/2;
 	if (f<0.2f) return 0xffffff;
 	if (f>0.8f) return 0x000000;
-	if (f<0.4f) return rgb(1.2-f,1.2-f,1.6-3*f);
-	if (f<0.6f) return rgb(1.2-f,2.0-3*f,1.6-3*f);
-	return rgb(2.4-3*f,2.0-3*f,1.6-3*f);
+	if (f<0.4f) return rgb(1.2f-f,1.2f-f,1.6f-3*f);
+	if (f<0.6f) return rgb(1.2f-f,2.0f-3*f,1.6f-3*f);
+	return rgb(2.4f-3*f,2.0f-3*f,1.6f-3*f);
 }
 
 void tile(int tx, int ty, float sc, int * p) {
--- src/tile.h
+++ src/tile.h
@@ -1,5 +1,5 @@
 #define TILE_SIZE 256
-#define TILES_X 6
-#define TILES_Y 4
+#define TILES_X 5
+#define TILES_Y 3
 void tile(int x, int y, float scale, int * pixels);
 void tile_init(int argc, char ** argv);
